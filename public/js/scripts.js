765e20412c60518190ce6549900b3ac18786acda
s-um <yoonms0101@gmail.com>
2021-07-24 00:12:54 +0900

    Add -swal password on start

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,48 +7,33 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+var pw = "daily"
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+function inputPW() {
+    swal({
+        title: "Warning",
+        content: "input",
+        text: "Input Password!",
+        button: {
+            text: "Submit",
+        },
+    }).then((result) => {
+        if (result == pw) {
+            swal({
+                title: "Successed",
+                text: "Access Complete",
+                icon: "success",
+            }).then(() => {
+                document.getElementsByClassName('hid')[0].className = "";
+            });
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            swal({
+                title: "Failed",
+                text: "Wrong Password",
+                icon: "error",
+            }).then(() => {
+                setTimeout(()=>{inputPW();}, 100);
+            });
         }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
+    })
+}
